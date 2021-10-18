09e6517ce81367445bffd8b9ec497066031a7b1c
Gustav Gyrst <gugy@itu.dk>
2021-10-18 17:11:09 +0200

    "minor change"

--- src/political_ads/helper.py
+++ src/political_ads/helper.py
@@ -1,5 +1,5 @@
 #Get gender distribution
-def get_gender_distribution(lst):
+def get_gender_distribution(lst:list):
     percentage = 0.0
     for item in lst:
         if item['gender'] == 'male':
@@ -7,7 +7,7 @@ def get_gender_distribution(lst):
     return percentage
 
 #Get age distribution
-def get_age_distribution(lst, all_ages=False):
+def get_age_distribution(lst:list, all_ages=False):
     age_dist = {}
     if all_ages == True:
         age_dist = {"13-17": 0.0, '18-24': 0.0, '25-34':0.0, '35-44':0.0, '45-54':0.0, '55-64':0.0, '65+':0.0}
