29e981f3e58e67c5729539784f6ed4b13d3e75f0
Gustav Gyrst <gugy@itu.dk>
2021-10-21 19:43:32 +0200

    smalll changes

--- src/political_ads/politician_filter.py
+++ src/political_ads/politician_filter.py
@@ -1,8 +1,10 @@
 import pandas as pd
+from facebook_scraper import get_page_info
+
 
 politicians_fb = pd.read_csv("src/data_sets/legislators-current.csv")
 
-def get_pol_fbpage_names(as_set=True):
+def get_pol_fbpage_names(as_set=False, as_list=False):
     
     pol_fb = politicians_fb['facebook']
     pol_fb.dropna(inplace=True)
@@ -15,6 +17,14 @@ def get_pol_fbpage_names(as_set=True):
             pol_fb_set.add(row)
         return pol_fb_set
     
+    if(as_list == True):
+        pol_fb_list = []
+
+        for row in pol_fb:
+            pol_fb_list.append(row)
+        
+        return pol_fb_list
+
     else:
         return pol_fb
 
@@ -35,6 +45,9 @@ def get_pol_full_names(as_set=True):
     else:
         return pol_fb
 
+list = get_pol_fbpage_names(as_list=True)
 
-test = get_pol_full_names()
-print(test)
+
+for i in list:
+    print(get_page_info(account=i))
+
