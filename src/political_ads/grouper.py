954342a4ad3602ad7840505ba09ff6d6084eaee8
Laurenz <laai@itu.dk>
2021-11-15 14:09:13 +0100

    some changes

--- src/political_ads/grouper.py
+++ src/political_ads/grouper.py
@@ -47,6 +47,28 @@ class Grouper:
 
         return by_day
 
+    # Takes as input dataframe containing ads and groups them by day
+    def group_ads_by_page(self, data: pd.DataFrame) -> pd.DataFrame:
+
+        by_page = data.groupby(["page_id", "page_name"]).agg(
+            # Aggregate no of ads
+            no_ads = ('id', 'count'),
+            # Aggregate sum of spend & total impressions generated
+            sum_spend_lo = ('spend_lo', 'sum'),
+            sum_spend_hi = ('spend_hi', 'sum'),
+            median_spend = ('spend', 'median'),
+            avg_spend = ('spend', 'sum'),
+            sum_impressions_lo = ('impressions_lo', 'sum'),
+            sum_impressions_hi = ('impressions_hi', 'sum'),
+            # Average number of impressions & spend per ad
+            avg_impressions = ('impressions', 'sum')
+        ).reset_index()
+
+        congress = pd.read_csv("..\\src\\data_sets\\legislators_fb_info_final.csv") # read congress member info
+        by_page["party"] = by_page["page_id"].apply(lambda x: congress[congress["page_id"] == x]["party"].values[0]) # add party to pages
+
+        return by_page
+
 
     # Takes as input dataframe containing ads and groups them by day
     def group_ads_by_party_by_day(self, data: pd.DataFrame, party: str) -> pd.DataFrame:
@@ -58,6 +80,19 @@ class Grouper:
         data_party_members = data[data.page_id.isin(party_members.page_id)] # get all the ads where page id matches with the party page id's
 
         data_party_by_day = self.group_ads_by_day(data_party_members) # group those ads by day
+
+        return data_party_by_day
+
+    # Takes as input dataframe containing ads and groups them by day
+    def group_ads_by_party_by_page(self, data: pd.DataFrame, party: str) -> pd.DataFrame:
+
+        congress = pd.read_csv("..\\src\\data_sets\\legislators_fb_info_final.csv") # read congress member info
+        
+        party_members = congress[congress["party"] == party]  # get all page id's that match the given party
+
+        data_party_members = data[data.page_id.isin(party_members.page_id)] # get all the ads where page id matches with the party page id's
+
+        data_party_by_day = self.group_ads_by_page(data_party_members) # group those ads by day
 
         return data_party_by_day
 
--- src/political_ads/keyword_filter.py
+++ src/political_ads/keyword_filter.py
@@ -8,12 +8,12 @@ class Filter:
     # returns true if ad IS climate related
     def check_for_climate_ad(self, text: str): # returns true if climate related keyword contained, false otherwise
         text_lower = text.lower()
-        return ("climate" in text_lower) | (("global" in text_lower) and ("warming" in text_lower)) and (("business climate" not in text_lower) | ("economic climate" not in text_lower)) 
+        return ("climate" in text_lower) | (("global" in text_lower) and ("warming" in text_lower)) and (("business climate" not in text_lower) & ("economic climate" not in text_lower) & ("biz climate" not in text_lower) & ("tax climate" not in text_lower) & ("regulatory climate" not in text_lower)) 
 
     # returns true if ad is NOT climate related (INVERSE of the above method!)
     def check_for_NON_climate_ad(self, text: str): # returns true if climate related keyword contained, false otherwise
         text_lower = text.lower()
-        return not ("climate" in text_lower) | (("global" in text_lower) and ("warming" in text_lower)) and (("business climate" not in text_lower) | ("economic climate" not in text_lower)) 
+        return not ("climate" in text_lower) | (("global" in text_lower) and ("warming" in text_lower)) and (("business climate" not in text_lower) & ("economic climate" not in text_lower) & ("biz climate" not in text_lower) & ("tax climate" not in text_lower) & ("regulatory climate" not in text_lower))  
 
     # main function to return dataframe with climate ads
     def get_climate_ads(self, data: pd.DataFrame) -> pd.DataFrame:
